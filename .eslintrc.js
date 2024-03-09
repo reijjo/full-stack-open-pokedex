0cd2ab84af127fcd0dc3acfac49209d827cb5d43
reijjo <teemu.aitomeri@gmail.com>
2024-03-09 21:16:11 +0200

    cypress part1

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     es6: true,
     node: true,
     "jest/globals": true,
+    "cypress/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
