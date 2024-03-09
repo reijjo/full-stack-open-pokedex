9f9117f208328045296afe3088425870d317ef73
reijjo <teemu.aitomeri@gmail.com>
2024-03-09 21:25:44 +0200

    cypress part2

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     "linebreak-style": ["error", "unix"],
     eqeqeq: "error",
