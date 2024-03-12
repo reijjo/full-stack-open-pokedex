dcd6ccf92c222c40f4d070981f866b0a70b1a351
reijjo <teemu.aitomeri@gmail.com>
2024-03-12 19:29:32 +0200

    health endpoint

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/version", (req, res) => {
   res.send("1");
 });
 
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
